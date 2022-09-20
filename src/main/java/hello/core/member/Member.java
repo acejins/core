1e8df0cb3c60c4dd33832dffb2bb3ce65059c60f
akjair <abubu24@nate.com>
2022-09-20 16:01:20 +0900

    20220920

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -1,6 +1,16 @@
 package hello.core.member;
 
 public class Member {
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
     public Long getId() {
         return id;
     }
@@ -24,8 +34,4 @@ public class Member {
     public void setGrade(Grade grade) {
         this.grade = grade;
     }
-
-    private Long id;
-    private String name;
-    private Grade grade;
 }
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,7 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
-    private final MemberRepository memberRepository = new
-            MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
     public void join(Member member) {
         memberRepository.save(member);
     }
